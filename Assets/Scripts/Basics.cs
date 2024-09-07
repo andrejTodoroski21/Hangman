306e79dc977311f4d437a5352c6b9ec35124f6d5
andrejTodoroski21 <106843061+andrejTodoroski21@users.noreply.github.com>
2024-09-06 20:06:27 -0400

    finished the hangman game

---
+++ Assets/Scripts/Basics.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Basics : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        string myName = "denis";
+        Debug.Log("THIS IS THE TEST AHHHHHH" + myName.Substring(3,2));
+    } 
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -2,27 +2,102 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.IO;
+
 
 public class GameController : MonoBehaviour
 {
 
     public Text timeField;
     public Text WordToFindField;
+    public GameObject[] HangMan;
+    public GameObject WinText;
+    public GameObject LoseText;
+    public GameObject RestartButton; 
     private float time;
-    private string[] wordsLocal = {"RICH", "MINECRAFT", "VALORANT", "PHANTOM"};
+    private static readonly string[] vs = File.ReadAllLines(@"Assets/words.txt");
+
+    //private string[] wordsLocal = {"RICH", "MINECRAFT", "VALORANT IS GOOD", "PHANTOM"};
+    private string[] words = vs;
     private string chosenWord;
     private string hiddenWord;
+    private int fails;
+    private bool gameEnd = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        chosenWord = wordsLocal[Random.Range(0,3)];
+
+        chosenWord = words[Random.Range(0, words.Length)];
+
+        for (int i = 0; i < chosenWord.Length; i++)
+        {
+            char letter = chosenWord[i];
+            Debug.Log(letter);
+
+            if (char.IsWhiteSpace(letter))
+            {
+                hiddenWord += " ";
+            }
+            else
+            {
+                hiddenWord += "_";
+            }
+
+        }
+        
+        WordToFindField.text = hiddenWord;
     }
 
     // Update is called once per frame
     void Update()
     {
-        time += Time.deltaTime;
-        timeField.text= time.ToString();
+        if (gameEnd == false)
+        {
+            time += Time.deltaTime;
+            timeField.text = time.ToString();
+        }
+    }
+    private void OnGUI()
+    {
+        Event e = Event.current;
+        if(e.type == EventType.KeyDown && e.keyCode.ToString().Length == 1)
+        {
+            string pressed = e.keyCode.ToString();
+            Debug.Log(pressed);
+            if (chosenWord.Contains(pressed))
+            {
+                int i = chosenWord.IndexOf(pressed);
+                while (i != -1)
+                {
+                    hiddenWord = hiddenWord.Substring(0, i) + pressed + hiddenWord.Substring(i + 1);
+                    Debug.Log(hiddenWord);
+
+                    chosenWord = chosenWord.Substring(0, i) + "_" + chosenWord.Substring(i + 1);
+                    Debug.Log(hiddenWord);
+                    i = chosenWord.IndexOf(pressed);
+                }
+
+                WordToFindField.text = hiddenWord;
+
+            }
+            else
+            {
+                HangMan[fails].SetActive(true);
+                fails++;
+            }
+            if(fails == HangMan.Length)
+            {
+                LoseText.SetActive(true);
+                RestartButton.SetActive(true);
+                gameEnd = true;
+            }
+            if (!hiddenWord.Contains("_"))
+            {
+                WinText.SetActive(true);
+                RestartButton.SetActive(true);
+                gameEnd = true;
+            }
+        }
     }
 }
